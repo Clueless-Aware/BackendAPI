f69940ca8eae6d1f7ada0bc8d91de1ea94c627cb
Angelo Bonicelli <angelo.bonicelli@fitstic-edu.com>
2023-01-20 17:55:58 +0100

    Moved models to new apps

---
+++ artApi/models/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=30, unique=True)
+    email = models.CharField(max_length=50)
+
+
+class Artwork(models.Model):
+    title = models.CharField(max_length=50)
+    artist = models.CharField(max_length=50, blank=True)
+    path_to_image = models.CharField(max_length=100)
