1c0240a378c584c20977db3ccb113e98e23a2045
Angelo Bonicelli <angelo.bonicelli@fitstic-edu.com>
2023-01-20 17:42:50 +0100

    Added Artwork model

--- artApi/artApi/models.py
+++ artApi/artApi/models.py
@@ -2,5 +2,11 @@ from django.db import models
 
 
 class User(models.Model):
-    Username = models.CharField(max_length=30)
-    Email = models.CharField(max_length=50)
+    username = models.CharField(max_length=30, unique=True)
+    email = models.CharField(max_length=50)
+
+
+class Artwork(models.Model):
+    title = models.CharField(max_length=50)
+    artist = models.CharField(max_length=50, blank=True)
+    path_to_image = models.CharField(max_length=100)
