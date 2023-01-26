fb55aaf11162e54e41740519a29d1795f1e275e1
Aiuola <angelo.bonicelli@fitstic-edu.com>
2023-01-26 11:55:10 +0100

    Added static root

--- artApi/artApi/settings.py
+++ artApi/artApi/settings.py
@@ -139,6 +139,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = "/backend/images/artworks"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
