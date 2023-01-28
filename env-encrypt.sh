8f9ea9ed6449580f62a9281a9b488c6ee891da0e
Aiuola <angelo.bonicelli@fitstic-edu.com>
2023-01-28 18:14:59 +0100

    New env file

--- env-encrypt.sh
+++ env-encrypt.sh
@@ -5,7 +5,7 @@ KEY=./.env.pass
 
 if [ -f "$KEY" ]; then
     npx senv encrypt .env > .env.encrypted
-    echo "Encription successful"
+    echo "Encryption successful"
 else
     echo "Missing pass file make sure you have file name '.env.pass'"
 fi
