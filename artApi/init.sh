3136a914663ffbeaac84e75eaaad1e7211dc978f
Aiuola <angelo.bonicelli@fitstic-edu.com>
2023-02-02 11:45:41 +0100

    fixed paths in script

--- artApi/init.sh
+++ artApi/init.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# Install dependencies at runtime
+# Installing dependencies at runtime
 pipenv install
 
 # Apply database migrations
 echo "Apply database migrations"
-pipenv run manage.py migrate
+pipenv run ./manage.py migrate
 
 # SuperUser
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ;
 then
-    pipenv run manage.py createsuperuser --no-input
+    pipenv run ./manage.py createsuperuser --no-input
 fi
 
 # Collect static files
 echo "Collect static files"
-pipenv run manage.py collectstatic --noinput
+pipenv run ./manage.py collectstatic --noinput
 
 
 # Start server
 echo "Starting server"
-pipenv run manage.py runserver 0.0.0.0:8000
+pipenv run ./manage.py runserver 0.0.0.0:8000
