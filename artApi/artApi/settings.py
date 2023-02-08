1babbf1f4b138eddbf3d45c4eb08b704a25e789c
Aiuola <angelo.bonicelli@fitstic-edu.com>
2023-02-08 17:34:13 +0100

    Changed static files location

--- artApi/artApi/settings.py
+++ artApi/artApi/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = "/backend/images/artworks"
+STATIC_ROOT = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
