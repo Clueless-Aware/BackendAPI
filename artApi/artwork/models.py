94fe7dfa389dca807c0eeaa8a1645fa90344882b
Aiuola <angelo.bonicelli@fitstic-edu.com>
2023-02-21 14:49:20 +0100

    Added foreign key in artwork model

--- artApi/artwork/models.py
+++ artApi/artwork/models.py
@@ -8,15 +8,7 @@ __all__ = ['Artwork', 'Artist']
 # Create your models here.
 def upload_to(instance, filename):
     _, file_extension = os.path.splitext(filename)
-    return 'images/artworks/{filename}'.format(filename=instance.title) + file_extension
-
-
-class Artwork(models.Model):
-    title = models.CharField(
-        max_length=80, blank=False, null=False)
-    description = models.TextField()
-    author = models.CharField(max_length=80, blank=False, null=True)
-    image_url = models.ImageField(upload_to=upload_to, blank=True, null=True)
+    return 'images/artworks/{filename}'.format(filename=instance.id) + file_extension
 
 
 class Artist(models.Model):
@@ -27,3 +19,13 @@ class Artist(models.Model):
     base = models.CharField(max_length=50)
     nationality = models.CharField(max_length=50)
     sourcePage = models.CharField(max_length=64, db_column='url')
+
+
+class Artwork(models.Model):
+    id = models.IntegerField(primary_key=True, db_column='ID')
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    picture_data = models.CharField(max_length=128)
+    file_info = models.CharField(max_length=50)
+    image_url = models.ImageField(upload_to=upload_to,
+                                  max_length=50, db_column='jpg url')
