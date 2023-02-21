9a5b991f242bbbdf3aa7a544bd05c92d40678f47
Aiuola <angelo.bonicelli@fitstic-edu.com>
2023-02-21 12:42:12 +0100

    Added a default pagination of 100

--- artApi/artApi/settings.py
+++ artApi/artApi/settings.py
@@ -161,6 +161,8 @@ CORS_ORIGIN_WHITELIST = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
     'DEFAULT_SCHEMA_CLASS':
         "drf_spectacular.openapi.AutoSchema",
     'DEFAULT_PERMISSION_CLASSES': [
