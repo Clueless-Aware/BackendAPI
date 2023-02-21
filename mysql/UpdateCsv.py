e1ad4dc6d97f702ce8391677de2780b21b3b3428
Angelo Bonicelli <angelo.bonicelli@fitstic-edu.com>
2023-02-21 22:35:55 +0100

    Possible column split

--- mysql/UpdateCsv.py
+++ mysql/UpdateCsv.py
@@ -1,10 +1,16 @@
 import pandas as pd
 
-df = pd.read_csv('artwork_source.csv')
-sample = df.head(500)
+HEAD_SIZE = 500
 
-sample['jpg url'] = sample['ID'].apply(lambda id: f'images/artworks/{id}.jpg')
+df = pd.read_csv('artwork_source.csv', nrows=HEAD_SIZE)
 
-print(sample)
+df['jpg url'] = df['ID'].apply(lambda image_id: f'images/artworks/{image_id}.jpg')
 
-sample.to_csv('./artwork.csv', index=False, )
+# df[['date', 'type', 'size', 'museum']] = df['picture_data'].str.split(', ', expand=True, n=3)
+# .apply(lambda x: x.str.strip())
+
+# print(df.to_string())
+
+df.to_csv('./artwork.csv', index=False)
+
+print(f'Successfully created a csv file with {HEAD_SIZE} elements')
