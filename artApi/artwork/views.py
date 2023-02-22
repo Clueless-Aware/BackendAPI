cdf7bfec6827ab5519e18010bf1062ca65ba4db4
Angelo Bonicelli <angelo.bonicelli@fitstic-edu.com>
2023-02-22 01:23:16 +0100

    Added filtering to artisti viewset

--- artApi/artwork/views.py
+++ artApi/artwork/views.py
@@ -1,12 +1,11 @@
-from django_filters import filters
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, permissions
+from rest_framework.filters import OrderingFilter
 from rest_framework.parsers import MultiPartParser, FormParser
 
 from .models import Artist, Artwork
 from .serializers import ArtistSerializer, ArtworkSerializer
 
-from rest_framework.filters import OrderingFilter
-from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 class ArtworkViewSet(viewsets.ModelViewSet):
@@ -15,10 +14,10 @@ class ArtworkViewSet(viewsets.ModelViewSet):
     parser_classes = (MultiPartParser, FormParser)
     # Filtering
     filter_backends = (DjangoFilterBackend, OrderingFilter,)
-    filterset_fields = ['title', 'description']
-    ordering_fields= '__all__'
-    ordering = ['title']
-    # Permission
+    filterset_fields = ['id', 'title']
+    ordering_fields = '__all__'
+    ordering = ['id']
+    # Permissions
     permission_classes = [permissions.IsAdminUser]
 
     def get_permissions(self):
@@ -28,19 +27,18 @@ class ArtworkViewSet(viewsets.ModelViewSet):
 
 
 class ArtistViewSet(viewsets.ModelViewSet):
-        queryset = Artist.objects.all()
-        serializer_class = ArtistSerializer
-        parser_classes = (MultiPartParser, FormParser)
-        # Filtering
-        filter_backends = (DjangoFilterBackend, OrderingFilter,)
-        filterset_fields = ['title', 'description']
-        ordering_fields = '__all__'
-        ordering = ['name']
-        # Permission
-        permissions_classes = [permissions.IsAdminUser]
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+    parser_classes = (MultiPartParser, FormParser)
+    # Filtering
+    filter_backends = (DjangoFilterBackend, OrderingFilter,)
+    filterset_fields = ['name', 'school']
+    ordering_fields = '__all__'
+    ordering = ['name']
+    # Permissions
+    permissions_classes = [permissions.IsAdminUser]
 
-        def get_permissions(self):
-            if self.request.method in ['GET', 'OPTIONS']:
-                return [permissions.AllowAny()]
-            return super().get_permissions()
-
+    def get_permissions(self):
+        if self.request.method in ['GET', 'OPTIONS']:
+            return [permissions.AllowAny()]
+        return super().get_permissions()
