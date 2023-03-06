d188e64c922ae60fca0ef2e248adf9a5d2bed261
Aiuola <angelo.bonicelli@fitstic-edu.com>
2023-03-06 17:49:28 +0100

    Fixed permissions for user model

--- artApi/users/views.py
+++ artApi/users/views.py
@@ -1,6 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, permissions
 from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.parsers import MultiPartParser, FormParser
 
 from .models import User
 from .serializers import UserSerializer
@@ -9,7 +10,7 @@ from .serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    # parser_classes = (MultiPartParser, FormParser)
+    parser_classes = (MultiPartParser, FormParser)
 
     # Filtering
     filter_backends = (DjangoFilterBackend, OrderingFilter, SearchFilter)
@@ -23,6 +24,4 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAdminUser]
 
     def get_permissions(self):
-        if self.request.method in ['POST']:
-            return [permissions.AllowAny()]
         return super().get_permissions()
