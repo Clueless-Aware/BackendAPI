f13bf9471db635ce5e5b8b46a57a2b6839465466
Angelo Bonicelli <angelo.bonicelli@fitstic-edu.com>
2023-03-08 00:08:57 +0100

    Changed the path of users profile pictures

--- artApi/users/models.py
+++ artApi/users/models.py
@@ -7,15 +7,17 @@ from django.db import models
 __all__ = ['User']
 
 
-def upload_to(instance, filename):
+# This somehow doesn't work... I AM AT WITS END
+def upload_to_user(instance, filename):
     _, file_extension = os.path.splitext(filename)
-    return 'images/users/{filename}'.format(filename=instance.username) + file_extension
+    return f'images/users/{instance.get_username()}' + file_extension
 
 
 class User(AbstractUser):
     biography = models.TextField(null=True)
     favorite_artist = models.ForeignKey(to=Artist, on_delete=models.CASCADE, null=True)
-    profile_picture = models.ImageField(upload_to=upload_to, default='images/users/default/unknown.jpg')
+    profile_picture = models.ImageField(upload_to='images/users/', max_length=256,
+                                        default='images/users/default/unknown.jpg')
 
     def __str__(self):
         return self.username
