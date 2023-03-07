261e6c57c0a094421676575eaf25b97dab0fd1dd
Aiuola <angelo.bonicelli@fitstic-edu.com>
2023-03-07 13:53:40 +0100

    Added Request model

--- artApi/account/models.py
+++ artApi/account/models.py
@@ -1,16 +1,29 @@
 # Create your models here.
 from artwork.models import Artwork
+from django.conf import settings
 from django.db import models
 from users.models import User
 
-__all__ = ['Favorite']
+__all__ = ['Favorite', 'Request']
 
 
 class Favorite(models.Model):
     account = models.ForeignKey(to=User, on_delete=models.CASCADE)
     artwork = models.ForeignKey(to=Artwork, on_delete=models.CASCADE)
 
-    date = models.DateField()
+    date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.account} - {self.artwork}'
+
+
+class Request(models.Model):
+    from_user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=64)
+    content = models.TextField(null=True)
+    critical = models.BooleanField(default=False)
+
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.subject} from {self.from_user}'
