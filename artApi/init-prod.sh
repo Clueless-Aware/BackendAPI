088ba3c0e1807e30841e31651283a763b700da2d
Ubuntu <azureuser@susana.weoiaz2gorku3jbhlatezba30b.zrhx.internal.cloudapp.net>
2023-03-18 10:49:57 +0000

    Add files to production mode

---
+++ artApi/init-prod.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Installing dependencies at runtime
+pipenv install
+
+# Apply database migrations
+
+echo "Apply database migrations"
+pipenv run ./manage.py migrate
+
+# SuperUser
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ;
+then
+    pipenv run ./manage.py createsuperuser --no-input
+fi
+
+# Collect static files
+echo "Collect static files"
+pipenv run ./manage.py collectstatic --noinput
+
+
+# Start server
+echo "Starting server"
+# Only on dev mode
+# pipenv run ./manage.py runserver 0.0.0.0:8000
+# On release
+pipenv run gunicorn artApi.wsgi:application --bind 0.0.0.0:8000
